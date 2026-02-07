bff2b8c2ca8a870bd4623cc10268f55930b4ed40
virtuallabFQ <marmotvirtuallab@gmail.com>
2026-02-07 21:31:34 +0000

    29

--- scripts/resolution_button.gd
+++ scripts/resolution_button.gd
@@ -1,4 +1,6 @@
-extends OptionButton
+class_name ResolutionButton extends OptionButton
+
+@export var window_button : WindowButton
 
 const screen_resolution_dictionary : Dictionary = {
 	"1024x576" : Vector2i(1024, 576),
@@ -13,10 +15,26 @@ const screen_resolution_dictionary : Dictionary = {
 func _ready():
 	add_screen_resolution_items()
 	self.item_selected.connect(on_screen_resolution_selected)
+	var screen_size = DisplayServer.screen_get_size()
+	DisplayServer.window_set_size(screen_size)
+	get_window().content_scale_size = screen_size
+	window_button.update_resolution_dropdown_visuals(screen_size)
 
 func add_screen_resolution_items() -> void:
 	for screen_resolution in screen_resolution_dictionary:
 		self.add_item(screen_resolution)
 
 func on_screen_resolution_selected(_index: int) -> void:
-	DisplayServer.window_set_size(screen_resolution_dictionary.values()[_index])
+	var selected_text = get_item_text(_index)
+	var size = screen_resolution_dictionary[selected_text]
+	apply_resolution(size)
+	
+func apply_resolution(size: Vector2i) -> void:
+	var current_mode = DisplayServer.window_get_mode()
+	get_window().content_scale_size = size
+	
+	if current_mode == DisplayServer.WINDOW_MODE_WINDOWED:
+		DisplayServer.window_set_size(size)
+		var screen_center = DisplayServer.screen_get_position() + DisplayServer.screen_get_size() / 2
+		var window_pos = screen_center - size / 2
+		DisplayServer.window_set_position(window_pos)
--- scripts/window_button.gd
+++ scripts/window_button.gd
@@ -1,8 +1,9 @@
-extends OptionButton
+class_name WindowButton extends OptionButton
+
+@export var resolution_button : ResolutionButton
 
 const window_mode_array : Array[String] = [
 	"Ecrã Cheio",
-	"Janela sem margens",
 	"Janela",
 ]
 
@@ -15,10 +16,34 @@ func add_window_mode_items() -> void:
 		self.add_item(window_mode)
 
 func on_window_mode_selected(index: int) -> void:
+	var screen_size = DisplayServer.screen_get_size()
+	
 	match index:
 		0:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+			resolution_button.disabled = false
+			DisplayServer.window_set_size(screen_size)
+			update_resolution_dropdown_visuals(screen_size)
 		1:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			resolution_button.disabled = true
+			DisplayServer.window_set_size(screen_size)
+			update_resolution_dropdown_visuals(screen_size)
 		2:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			resolution_button.disabled = false
+			var target_size = Vector2i(1024, 576)
+			DisplayServer.window_set_size(target_size)
+			var screen_center = DisplayServer.screen_get_position() + DisplayServer.screen_get_size() / 2
+			var window_pos = screen_center - target_size / 2
+			DisplayServer.window_set_position(window_pos)
+			update_resolution_dropdown_visuals(target_size)
+			
+func update_resolution_dropdown_visuals(size: Vector2i):
+	var expected_text = str(size.x) + "x" + str(size.y) 
+	for i in range(resolution_button.item_count):
+		var item_text = resolution_button.get_item_text(i)
+		if item_text.replace(" ", "") == expected_text:
+			resolution_button.selected = i
+			return
+		
