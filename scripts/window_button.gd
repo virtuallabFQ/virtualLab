4ab19d7a4033979d1a6e21ae8ec14b32d5393a7c
virtuallabFQ <marmotvirtuallab@gmail.com>
2026-02-08 01:20:32 +0000

    31

--- scripts/window_button.gd
+++ scripts/window_button.gd
@@ -7,8 +7,20 @@ var window_mode_array : Array[String] = [
 	"Janela",
 ]
 
+func check_variables():
+	var window = get_window()
+	var mode = window.get_mode()
+
+	if mode == Window.MODE_FULLSCREEN:
+		selected = 0
+		resolution_button.disabled = true
+	elif mode == Window.MODE_WINDOWED:
+		selected = 1
+		resolution_button.disabled = false
+
 func _ready():
 	add_window_mode_items()
+	check_variables()
 	item_selected.connect(on_window_mode_selected)
 
 func add_window_mode_items() -> void:
@@ -19,7 +31,7 @@ func on_window_mode_selected(index: int) -> void:
 	match index:
 		0:
 			get_window().set_mode(Window.MODE_FULLSCREEN)
-			resolution_button.disabled = false
+			resolution_button.disabled = true
 			var screen_size = DisplayServer.screen_get_size()
 			get_window().set_size(screen_size)
 			update_resolution(screen_size)
