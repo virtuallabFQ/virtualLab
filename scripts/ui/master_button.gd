ad7e3a149907d990c6e8373ba912d79696b7f6c9
virtuallabFQ <marmotvirtuallab@gmail.com>
2026-02-10 14:11:52 +0000

    44

--- scripts/ui/master_button.gd
+++ scripts/ui/master_button.gd
@@ -5,6 +5,7 @@ class_name MasterButton extends Button
 
 func _ready():
 	scale_slider.value_changed.connect(on_slider_value_changed)
+	update_label_text(scale_slider.value)
 
 func on_slider_value_changed(value):
 	if value == -45:
@@ -12,3 +13,9 @@ func on_slider_value_changed(value):
 	else:
 		AudioServer.set_bus_mute(0,false)
 	AudioServer.set_bus_volume_db(0,value)
+	update_label_text(value)
+	
+func update_label_text(value):
+	var range_length = scale_slider.max_value - scale_slider.min_value
+	var percent = (value - scale_slider.min_value) / range_length * 100
+	scale_label.text = str(int(percent)) + "%"
--- scripts/ui/music_button.gd
+++ scripts/ui/music_button.gd
@@ -1,1 +1,22 @@
-extends Button
+class_name MusicButton extends Button
+
+@onready var scale_slider = $Slider
+@onready var scale_label = $Label
+
+func _ready():
+	scale_slider.value_changed.connect(on_slider_value_changed)
+	update_label_text(scale_slider.value)
+
+func on_slider_value_changed(value):
+	var busindex = AudioServer.get_bus_index("Music")
+	AudioServer.set_bus_volume_db(busindex, value)
+	if value == -45:
+		AudioServer.set_bus_mute(busindex,true)
+	else:
+		AudioServer.set_bus_mute(busindex,false)
+	update_label_text(scale_slider.value)
+
+func update_label_text(value):
+	var range_length = scale_slider.max_value - scale_slider.min_value
+	var percent = (value - scale_slider.min_value) / range_length * 100
+	scale_label.text = str(int(percent)) + "%"
