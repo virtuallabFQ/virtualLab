c3d72622f8f228bd48cf883aa819afd9bf1061bb
virtuallabFQ <marmotvirtuallab@gmail.com>
2026-02-15 21:23:10 +0000

    62

--- scripts/objects/interaction_raycast.gd
+++ scripts/objects/interaction_raycast.gd
@@ -5,19 +5,16 @@ var is_holding := false
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"interact"): 
-		is_holding = true
-		_interact(true, false)
+		is_holding = true; _interact(true, false)
 	elif event.is_action_released(&"interact"): 
-		is_holding = false
-		_interact(false, true)
+		is_holding = false; _interact(false, true)
 
 func _physics_process(_delta: float) -> void:
-	var held: Node3D = Global.player.held_object if Global.player else null
+	var held := Global.player.held_object if Global.player else null
 	var hit := get_collider() as Node3D; if hit == held: hit = null
 	
 	if hit != target:
 		if target:
-			if is_holding and target.has_method(&"interact_draw"): target.interact_draw(Vector3.ZERO, false, true)
 			if held: held.propagate_call(&"cancel_tool", [target])
 			if target.has_user_signal(&"unfocused"): target.emit_signal(&"unfocused")
 			MessageBus.interaction_unfocused.emit()
@@ -28,7 +25,7 @@ func _physics_process(_delta: float) -> void:
 	elif target and target.has_method(&"interact_ui"): target.interact_ui(hit_pt, false, false)
 
 func _interact(pressed: bool, released: bool) -> void:
-	var held: Node3D = Global.player.held_object if Global.player else null
+	var held := Global.player.held_object if Global.player else null
 	_interact_action(held, get_collision_point() if target else Vector3.ZERO, pressed, released)
 
 func _interact_process(held: Node3D, hit_pt: Vector3) -> void:
