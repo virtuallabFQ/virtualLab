6cc1cc3cce161246e3c039ca363d3abdabaf92cd
virtuallabFQ <marmotvirtuallab@gmail.com>
2026-02-15 18:26:12 +0000

    60

--- scripts/objects/scene_change_component.gd
+++ scripts/objects/scene_change_component.gd
@@ -1,10 +1,7 @@
 class_name SceneChangeComponent extends Node
 
-@export var interaction_component: InteractionComponent
-@export_file("*.tscn") var target_scene_path: String
+@export var interact_component: InteractionComponent
+@export_file("*.tscn") var scene_path: String
 
 func _ready() -> void:
-	interaction_component.player_interacted.connect(on_interact)
-	
-func on_interact(_body) -> void:
-	Global.game_controller.change_gui_scene(target_scene_path)
+	interact_component.player_interacted.connect(func(_target): Global.game_controller.change_gui_scene(scene_path))
