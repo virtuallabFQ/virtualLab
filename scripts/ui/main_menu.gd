c2907804fcbce9b245ea1bd9df9713fe8cffff59
virtuallabFQ <marmotvirtuallab@gmail.com>
2026-02-16 17:17:01 +0000

    70

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,46 +1,40 @@
-extends Control
+class_name MainMenu extends Control
 
-@export var menu : Control
-@export var options : Control
-@export var animation_player : AnimationPlayer
+enum State {MENU, OPTIONS}
 
-enum state {menu, options}
-var ui_state = state.menu
+@export var menu: Control
+@export var options: Control
+@export var animation_player: AnimationPlayer
+
+var ui_state := State.MENU
 
 func _ready() -> void:
-	options.back_to_main_menu.connect(on_options_back)
+	if options and options.has_signal(&"back_to_main_menu"): 
+		options.back_to_main_menu.connect(on_options_back)
 
-func _input(event):
-	if event.is_action_pressed("ui_cancel") and not animation_player.is_playing():
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"ui_cancel") and not animation_player.is_playing():
 		get_viewport().set_input_as_handled()
-		
-		match ui_state:
-			state.menu:
-				pass
-			state.options:
-				if options.has_method("on_exit_submenu"):
-					options.on_exit_submenu()
+		if ui_state == State.OPTIONS and options.has_method(&"on_exit_submenu"): 
+			options.on_exit_submenu()
 
-func on_options_back():
-	ui_state = state.menu
-	hide_and_show("options", "menu")
+func on_options_back() -> void:
+	ui_state = State.MENU
+	hide_and_show(&"hide_options", &"show_menu")
 
-func hide_and_show(first: String, second: String):
-	animation_player.play("hide_" + first)
-	await animation_player.animation_finished
-	animation_player.play("show_" + second)
+func hide_and_show(first: StringName, second: StringName) -> void:
+	animation_player.play(first)
+	await animation_player.animation_finished; animation_player.play(second)
 
 func on_start_button_pressed() -> void:
-	ui_state = state.menu
-	if animation_player.is_playing():
-		return
-	animation_player.play("hide_menu")
+	if animation_player.is_playing(): return
+	ui_state = State.MENU; animation_player.play(&"hide_menu")
 	await animation_player.animation_finished
 	Global.game_controller.change_gui_scene("res://scenes/levels/lab.tscn")
 
 func on_options_button_pressed() -> void:
-	ui_state = state.options
-	hide_and_show("menu", "options")
+	if animation_player.is_playing(): return
+	ui_state = State.OPTIONS; hide_and_show(&"hide_menu", &"show_options")
 
 func on_exit_button_pressed() -> void:
 	get_tree().quit()
--- scripts/ui/options_menu.gd
+++ scripts/ui/options_menu.gd
@@ -1,59 +1,53 @@
-extends Control
+class_name OptionsMenu extends Control
 
 signal back_to_main_menu
 
-@export var buttons : Control
-@export var graphics_menu : Control
-@export var audio_menu : Control
-@export var camera_menu : Control
-@export var animation_player : AnimationPlayer
+@export var buttons: Control
+@export var graphics_menu: Control
+@export var audio_menu: Control
+@export var camera_menu: Control
+@export var animation_player: AnimationPlayer
 
-enum substate {options, camera, graphics, audio}
-var ui_state = substate.options
+enum Substate {OPTIONS, CAMERA, GRAPHICS, AUDIO}
+var ui_state := Substate.OPTIONS
 
-@onready var menu_map = {
-	substate.options: {"node": buttons, "anim": "options"},
-	substate.camera: {"node": camera_menu, "anim": "camera"},
-	substate.graphics: {"node": graphics_menu, "anim": "graphics"},
-	substate.audio: {"node": audio_menu, "anim": "audio"}
+@onready var menu_map: Dictionary = {
+	Substate.OPTIONS: {"node": buttons, "hide": &"hide_options", "show": &"show_options"},
+	Substate.CAMERA: {"node": camera_menu, "hide": &"hide_camera", "show": &"show_camera"},
+	Substate.GRAPHICS: {"node": graphics_menu, "hide": &"hide_graphics", "show": &"show_graphics"},
+	Substate.AUDIO: {"node": audio_menu, "hide": &"hide_audio", "show": &"show_audio"}
 }
 
 func _ready() -> void:
 	for state_key in menu_map:
-		var menu_node = menu_map[state_key]["node"]
-		if menu_node: menu_node.visible = (state_key == substate.options)
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
-		get_viewport().set_input_as_handled()
-		on_exit_submenu()
+		var menu_node: Control = menu_map[state_key]["node"]
+		if menu_node: menu_node.visible = (state_key == Substate.OPTIONS)
 
 func on_exit_submenu() -> bool:
 	if animation_player.is_playing(): return true
-	if ui_state == substate.options:
+	
+	if ui_state == Substate.OPTIONS:
 		back_to_main_menu.emit()
 		return false
-	change_sub_menu(substate.options)
+	change_sub_menu(Substate.OPTIONS)
 	return true
 
-func change_sub_menu(new_state: substate) -> void:
+func change_sub_menu(new_state: Substate) -> void:
 	if animation_player.is_playing() or ui_state == new_state: return
-	var old_anim = menu_map[ui_state]["anim"]
-	var new_anim = menu_map[new_state]["anim"]
+	
+	var old_state := ui_state
 	ui_state = new_state
-	animation_player.play("hide_" + old_anim)
+	animation_player.play(menu_map[old_state]["hide"])
 	await animation_player.animation_finished
 	
 	for state_key in menu_map:
-		if menu_map[state_key]["node"]:
-			menu_map[state_key]["node"].visible = (state_key == new_state)
-	animation_player.play("show_" + new_anim)
+		var node: Control = menu_map[state_key]["node"]
+		if node: node.visible = (state_key == new_state)
+	animation_player.play(menu_map[new_state]["show"])
 
 func on_camera_controls_button_pressed() -> void:
-	change_sub_menu(substate.camera)
-
+	change_sub_menu(Substate.CAMERA)
 func on_graphic_button_pressed() -> void:
-	change_sub_menu(substate.graphics)
-
+	change_sub_menu(Substate.GRAPHICS)
 func on_audio_button_pressed() -> void:
-	change_sub_menu(substate.audio)
+	change_sub_menu(Substate.AUDIO)
